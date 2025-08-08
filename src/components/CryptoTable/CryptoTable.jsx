defe6be28a0e03cbdda25f944499838eedbc9e25
Poorvith <poorvithgowda10@gmail.com>
2025-08-08 21:11:05 +1000

    sort logic

--- src/components/CryptoTable/CryptoTable.jsx
+++ src/components/CryptoTable/CryptoTable.jsx
@@ -3,9 +3,11 @@ import { useEffect, useState } from "react";
 import useCryptoData from "../../hooks/useCryptoData";
 import CryptoCard from "../CryptoCard/CryptoCard";
 
-const CryptoTable =( {searchTerm} )=>{
+const CryptoTable = ({searchTerm}) => {
     const [cryptoRes, setCryptoRes]= useState([]);
     const [err,setErr]=useState('');
+    const [sortField, setSortField] = useState('market_cap');
+    const [sortDirection, setSortDirection] = useState('desc');
     const {isLoading,isError,handleCoinGekcoApi} = useCryptoData ();
 
     useEffect(()=>{
@@ -24,10 +26,36 @@ const CryptoTable =( {searchTerm} )=>{
         setCryptoRes(c);
     }
 
+    const handleSort = (fieldName) => {
+    setSortField(fieldName);
+    let sortedResult = [...cryptoRes];
+    if (sortDirection === 'desc') {
+        setSortDirection('asc');
+        sortedResult.sort((a, b) => {
+            if (typeof a[fieldName] === 'string') {
+                return a[fieldName].localeCompare(b[fieldName]);
+            }
+            return a[fieldName] - b[fieldName];
+        });
+    } else {
+        setSortDirection('desc');
+        sortedResult.sort((a, b) => {
+            if (typeof a[fieldName] === 'string') {
+                return b[fieldName].localeCompare(a[fieldName]);
+            }
+            return b[fieldName] - a[fieldName];
+        });
+    }
+    setCryptoRes(sortedResult);
+};
+
     return (
         <div>
             {isError  && <div>{isError}</div>}
             <button onClick={handleSearch}>Click</button>
+            <button onClick={()=>handleSort('name')}>Name</button>
+            <button onClick={()=>handleSort('current_price')}>Price</button>
+            <button onClick={()=>handleSort('market_cap')}>Market Cap</button>
             {cryptoRes.length>0 && (
                 <ol>
                     {cryptoRes.map((crypto,key)=>
